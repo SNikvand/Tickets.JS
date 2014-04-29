6e9415d5e55f5c86102989b4320873e0f1494f32
fuzzymodulo <fuzzymodulo@gmail.com>
2014-04-29 15:32:38 -0700

    Last commit before rebase. Added comments to reply functions.

--- public/javascripts/ticket_client.js
+++ public/javascripts/ticket_client.js
@@ -1,1 +1,17 @@
 var socket = io.connect();
+
+$(document).ready(function() {
+    /*var mainList;
+    var expiredList;
+    var dueList;
+
+    var mainFilters = {user: null, dept: null, priority: null, email: null};
+    var expiredFilters = {user: null, dept: null, priority: null, email: null};
+    var dueFilters = {user: null, dept: null, priority: null, email: null};
+
+    // get full list
+    socket.emit('getTickets', mainFilters, null, 1);
+    socket.on('displayTickets', function(ticketList) {
+
+    });*/
+});
