a168c2dc589c70a75788a409e3555dc41b9a4654
fuzzymodulo <fuzzymodulo@gmail.com>
2014-04-30 04:15:24 -0700

    The home page checks the session role and the page loads different elements accordingly (for now, just basic welcome messages and different ticket lists).

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -3,6 +3,16 @@ var expiredList;
 var dueList;
 
 function initializeAdmin() {
+    // DATABASE FUNCTION: number of new tickets should somehow be fetched from database
+    $('#newTickets').html('New tickets since last login: <a href="#">10</a>');
+    $('#newTickets').css('display', 'block');
+
+    // DATABASE FUNCTION: number of total tickets should also be fetched from database
+    $('#totalTickets').html('Total number of tickets: <a href="#">600</a>');
+    $('#totalTickets').css('display', 'block');
+
+    $('#adminHome').css('display', 'block');
+
     var mainFilters = {dept: null, priority: null, submittedBy: null, clientEmail: null,
         assignedTo: null, alteredBy: null, dateCreated: null, dateAltered: null};
     var expiredFilters = {dept: null, priority: null, submittedBy: null, clientEmail: null,
@@ -13,12 +23,36 @@ function initializeAdmin() {
     // get full list
     socket.emit('getTickets', mainFilters, null, "include", "exclude", 5);
     socket.on('displayTickets', function(ticketList) {
-
+        
     });
 }
 
+function initializeManager() {
+    // DATABASE FUNCTION: number of new tickets for this user's departments
+    $('#newTickets').html('New tickets in your departments since last login: <a href="#">10</a>');
+    $('#newTickets').css('display', 'block');
+
+    // DATABASE FUNCTION: number of total tickets fetched for this user's departments
+    $('#totalTickets').html('Total tickets in your departments: <a href="#">32</a>');
+    $('#totalTickets').css('display', 'block');
+
+    $('#managerHome').css('display', 'block');
+}
+
+function initializeITUser() {
+    // DATABASE FUNCTION: number of newly assigned tickets for this user
+    $('#newAssigned').html('New ticket assignments since last login: <a href="#">1</a>');
+    $('#newAssigned').css('display', 'block');
+
+    // DATABASE FUNCTION: total number of assigned tickets for this user
+    $('#totalAssigned').html('Total number of assignments: <a href="#">5</a>');
+    $('#totalAssigned').css('display', 'block');
+
+    $('#itUserHome').css('display', 'block');
+}
+
 $(document).ready(function() {
-    $('#welcomeUser').html('Welcome, ' + sessionUser + '!');
+    $('#welcomeUser').html('Welcome back, ' + sessionUser + '!');
 
     switch (sessionRole) {
         case "IT User":
