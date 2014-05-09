b55bb91f1894a3bc1eafee0a4a24b9e58e8865ce
Chris <chris.katana80@gmail.com>
2014-05-09 15:16:21 -0700

    portal setup and working.  waiting on submition to work.  email added at THIS commit.

--- public/javascripts/client-module.js
+++ public/javascripts/client-module.js
@@ -1,6 +1,3 @@
-/**
- * Created by Chris on 08/05/2014.
- */
 var clientModule = angular.module('client', ['ngRoute']);
 
 
@@ -34,27 +31,12 @@ clientModule.controller('viewticketController', function($scope) {
 
 });
 
-// this directive controls the drop-down options on the new ticket page.
-clientModule.directive('newTicket', function($location) {
-    return {
-        restrict: 'E',
-        link: function(scope, element, attrs) {
-            scope.isCompletedDisabled = function() {
-                if (scope.expiredSelection === 'onlyExpired') {
-                    scope.completedSelection = 'excludeCompleted';
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-
-            $(".dropdown-toggle").on('click', function() {
-                $(".dropdown-menu li a").click(function(){
-                    $(".btn:first-child").text($(this).text());
-                    $(".btn:first-child").val($(this).text());
-                    scope.priority = $(this).text();
-                });
-            });
-        }
-    }
+clientModule.directive('newTicket', function() {
+    $(".dropdown-toggle").on('click', function() {
+        $(".dropdown-menu li a").click(function(){
+            $(".btn:first-child").text($(this).text());
+            $(".btn:first-child").val($(this).text());
+            scope.priority = $(this).text();
+        });
+    });
 });
