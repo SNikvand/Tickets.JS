cc4a8f1ecf9c907d42800f1875d6cdc4844ed63a
Chris <chris.katana80@gmail.com>
2014-05-09 16:02:25 -0700

    Commenting added and general look of the code cleaned up

--- public/javascripts/client-module.js
+++ public/javascripts/client-module.js
@@ -1,7 +1,6 @@
 var clientModule = angular.module('client', ['ngRoute']);
 
-
-
+//routing configuration for the portal page.
 clientModule.config(function($routeProvider,$locationProvider) {
     $routeProvider
         .when('/newticket', {
@@ -12,14 +11,17 @@ clientModule.config(function($routeProvider,$locationProvider) {
             templateUrl: '/partials/client/viewticket.html',
             controller: 'viewticketController'
         })
-        .otherwise({
+        .when('/viewticket', {      // route sent to after ticket creation.  May be a placeholder till hash is working.
+            templateUrl: '/partials/client/viewticket.html',
+            controller: 'viewticketController'
+        })
+        .otherwise({      // redirect if improper route entered.
             redirectTo: '/newticket'
         });
 });
 
-// new ticket controller
 clientModule.controller('newticketController', function($scope, $location) {
-    $scope.submit = function() { // when submit is pressed sends the data in the fields
+    $scope.submit = function() {
         socket.emit('setTicket', null, $scope.title, $scope.dept, $scope.body, $scope.priority, $scope.user, $scope.email,
             null, null, null, null, null, null, false);
 
@@ -31,12 +33,27 @@ clientModule.controller('viewticketController', function($scope) {
 
 });
 
-clientModule.directive('newTicket', function() {
-    $(".dropdown-toggle").on('click', function() {
-        $(".dropdown-menu li a").click(function(){
-            $(".btn:first-child").text($(this).text());
-            $(".btn:first-child").val($(this).text());
-            scope.priority = $(this).text();
-        });
-    });
+// this directive controls the drop-down options on the new ticket page.
+clientModule.directive('newTicket', function($location) {
+    return {
+        restrict: 'E',
+        link: function(scope, element, attrs) {
+            scope.isCompletedDisabled = function() {
+                if (scope.expiredSelection === 'onlyExpired') {
+                    scope.completedSelection = 'excludeCompleted';
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+            $(".dropdown-toggle").on('click', function() {
+                $(".dropdown-menu li a").click(function(){
+                    $(".btn:first-child").text($(this).text());
+                    $(".btn:first-child").val($(this).text());
+                    scope.priority = $(this).text();
+                });
+            });
+        }
+    }
 });
