f15b3efe6f51ff94d76747e5d98c4a82f2f901e2
Chris <chris.katana80@gmail.com>
2014-05-09 15:16:21 -0700

    portal setup and working.  waiting on submition to work.  email added at THIS commit.

--- public/javascripts/client-module.js
+++ public/javascripts/client-module.js
@@ -2,6 +2,7 @@ var clientModule = angular.module('client', ['ngRoute']);
 
 
 
+
 clientModule.config(function($routeProvider,$locationProvider) {
     $routeProvider
         .when('/newticket', {
@@ -12,17 +13,22 @@ clientModule.config(function($routeProvider,$locationProvider) {
             templateUrl: '/partials/client/viewticket.html',
             controller: 'viewticketController'
         })
+        .when('/viewticket', {
+            templateUrl: '/partials/client/viewticket.html',
+            controller: 'viewticketController'
+        })
         .otherwise({
             redirectTo: '/newticket'
         });
 });
 
-clientModule.controller('newticketController', function($scope) {
+clientModule.controller('newticketController', function($scope, $location) {
     $scope.submit = function() {
         socket.emit('setTicket', null, $scope.title, $scope.dept, $scope.body, $scope.priority, $scope.user, $scope.email,
             null, null, null, null, null, null, false);
 
-        $location.path('/viewtickets');
+        $location.path('/viewticket');
+
     }
 });
 
@@ -30,12 +36,26 @@ clientModule.controller('viewticketController', function($scope) {
 
 });
 
-clientModule.directive('newTicket', function() {
-    $(".dropdown-toggle").on('click', function() {
-        $(".dropdown-menu li a").click(function(){
-            $(".btn:first-child").text($(this).text());
-            $(".btn:first-child").val($(this).text());
-            scope.priority = $(this).text();
-        });
-    });
+clientModule.directive('newTicket', function($location) {
+    return {
+        restrict: 'E',
+        link: function(scope, element, attrs) {
+            scope.isCompletedDisabled = function() {
+                if (scope.expiredSelection === 'onlyExpired') {
+                    scope.completedSelection = 'excludeCompleted';
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+            $(".dropdown-toggle").on('click', function() {
+                $(".dropdown-menu li a").click(function(){
+                    $(".btn:first-child").text($(this).text());
+                    $(".btn:first-child").val($(this).text());
+                    scope.priority = $(this).text();
+                });
+            });
+        }
+    }
 });
